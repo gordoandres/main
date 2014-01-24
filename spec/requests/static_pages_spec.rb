acb6bd7dc2d1cd0810389dfa673a0ee81545f575
mariohernandezv <mariohernandezv@yahoo.com.ar>
2014-01-23 22:15:13 -0500

    Agregar un full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,19 +2,33 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Openii" }
+
   describe "Pagina de inicio" do
 
-    it "debe tener el texto 'Inicio'" do
+    it "debe tener el texto 'Openii'" do
       visit '/static_pages/home'
-      expect(page).to have_content("Inicio")
+      expect(page).to have_content("Openii")
     end
+
+    it "debe tener el titulo 'Inicio'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title} | Inicio")
+    end
+
   end
 
   describe "Pagina de ayuda" do 
+
   	it "debe tener el texto 'Ayuda'" do
 	  	visit '/static_pages/help'
   		expect(page).to have_content('Ayuda')
   	end
+    it "debe tener el titulo 'Ayuda'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Ayuda")
+    end
+
   end
 
   describe "Acerca de" do 
@@ -22,6 +36,13 @@ describe "Static pages" do
   		visit '/static_pages/acerca_de'
   		expect(page).to have_content('Acerca de')
   	end
+
+    it "debe tener el titulo 'Acerca de'" do
+      visit '/static_pages/acerca_de'
+      expect(page).to have_title("#{base_title} | Acerca de")
+    end
+
   end
+
 
 end
