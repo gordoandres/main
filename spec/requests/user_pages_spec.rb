5271d89153dbc52eb02c253947df5e06148b6dcb
mariohernandezv <mariohernandezv@yahoo.com.ar>
2014-01-24 23:43:42 -0500

    Corrigiendo detalles del layout

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,8 +6,8 @@ describe "Paginas de usuarios" do
 	describe "pagina de registro" do
 		before { visit registro_path }
 
-		it { should have_content('Registrate') }
-		it { should have_title(full_title('Registrate')) }
+		it { should have_content('Regístrate') }
+		it { should have_title(full_title('Regístrate')) }
 	end
 
 end
