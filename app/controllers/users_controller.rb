6ecb5cc297bc09504c139c15f315592ce5420a50
mariohernandezv <mariohernandezv@yahoo.com.ar>
2014-01-25 20:03:26 -0500

    puliendo el codigo de registro

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-      flash[:succes] = "Bienvenido a Openii!"
+      flash[:success] = "Bienvenido a Openii!"
       redirect_to @user
   	else
   		render 'new'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,9 +21,18 @@ describe "Páginas de usuarios" do
     let(:submit) { "Crear mi cuenta" }
 
     describe "con informacion invalida" do
+
       it "no debe crear un usuario" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "despues de submit" do
+        before { click_button submit }
+
+        it { should have_title('Regístrate') }
+        it { should have_content('error') }
+      end
+
     end
 
     describe "con informacion valida" do
@@ -32,6 +41,14 @@ describe "Páginas de usuarios" do
         fill_in "Email",  with: "usuario@ejemplo.com"
         fill_in "Password",  with: "foobar"
         fill_in "Confirma el password",   with: "foobar" 
+      end
+
+      describe "luego de salvar al usuario" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'usuario@ejemplo.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Bienvenido') }
       end
 
       it "debe crear un usuario" do
