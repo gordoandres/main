aed7e8f0475f86ddbf1dd1966466d67591ef50ec
mariohernandezv <mariohernandezv@yahoo.com.ar>
2014-02-10 22:45:47 -0500

    Primer Form de contacto

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Main::Application.configure do
   config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
-    address: "mail.domain.com",
+    address: "smtp.gmail.com",
     port: 587, 
     authentication: "plain",
     user_name: "mhernand78@gmail.com",
