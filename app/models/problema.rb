cfe11155c7a293fad78f21bd6377ba2398d98490
mariohernandezv <mariohernandezv@yahoo.com.ar>
2014-03-09 10:12:26 -0500

    Titulo porblema unico

--- app/models/problema.rb
+++ app/models/problema.rb
@@ -1,6 +1,12 @@
 class Problema < ActiveRecord::Base
 	belongs_to :proyect
-	validates :problema, presence: true, length: { maximum: 140 }
-	validates :descripcion, presence: true, length: { maximum: 560 }
-	validates :proyect_id, presence: true
+	validates 	:problema, 
+				presence: true, 
+				length: { maximum: 140 }, 
+				uniqueness: { case_sensitive: false }
+	validates 	:descripcion, 
+				presence: true, 
+				length: { maximum: 560 }
+	validates 	:proyect_id, 
+				presence: true
 end
